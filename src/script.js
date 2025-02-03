0abcc47b854dfb510b88b05d46dfb4613b75f65e
Kanakosaitoh <kanahanasasha@gmail.com>
2025-02-03 14:23:48 -0800

    added function and typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
+function generatePoem(event) {
+  event.preventDefault();
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = "poem";
 
+  new Typewriter("#poem", {
+    strings: ["Hello"],
+    autoStart: true,
+    delay: 100,
+    cursor: " ",
+  });
+}
+let textElement = document.querySelector("#text-element");
+textElement.addEventListener("submit", generatePoem);
