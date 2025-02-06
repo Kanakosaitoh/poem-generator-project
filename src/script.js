9f6672c4d93b4dec1d7952c5d4bc7d8a6190a46f
Kanakosaitoh <kanahanasasha@gmail.com>
2025-02-06 09:10:31 -0800

    changed h1
    added "Canadian" to AI Poem Generator

--- src/script.js
+++ src/script.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
   let apiKey = "821b2a703ac1t98f96395477546f6ebo";
   let prompt = `Generate a canadian poem about ${instructionInput.value}`;
   let context =
-    "make a poem in short 4 lines with <br /> between lines in HTML style";
+    "make a Canadian poem in short 4 lines with <br /> between lines in HTML style";
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   console.log("generating poem");
