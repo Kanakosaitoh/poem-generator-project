e2bcbda081aa1022797d29d2643d39dfb071678f
Kanakosaitoh <kanahanasasha@gmail.com>
2025-02-06 14:00:00 -0800

    Improved user experience

--- src/script.js
+++ src/script.js
@@ -13,11 +13,15 @@ function generatePoem(event) {
   let instructionInput = document.querySelector("#user-instruction");
 
   let apiKey = "821b2a703ac1t98f96395477546f6ebo";
-  let prompt = `Generate a canadian poem about ${instructionInput.value}`;
+  let prompt = `Generate a Canadian poem about ${instructionInput.value}`;
   let context =
     "You are a smart AI and like to make a short 4-line-Canadian poem with <br /> between lines in HTML style. please refrain from using backticks or a word of html in the poem.";
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink_me">Generating a Canadian poem about ${instructionInput.value}...</div>`;
   console.log("generating poem");
   console.log(`prompt:${prompt}`);
   console.log(`context:${context}`);
